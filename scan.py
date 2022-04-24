7247283a1c1bce14a50751de9e35a207707a20b6
Mr robot <30386159+edmark21@users.noreply.github.com>
2022-04-25 01:50:40 +0800

    Update scan.py

--- scan.py
+++ scan.py
@@ -139,7 +139,56 @@ def scan2():
           if counter:
             total+=1
 
-####################################################            
+####################################################  
+def scan3():
+  path = "team"
+  co = os.listdir(path)
+  pf = "team/"
+  print("Please wait...")
+  f = open('acc.txt')
+  n = f.readlines()
+  name = n[0]
+  res = " " in name
+  mao = name.split()[0]
+  API2 = "https://api2.splinterlands.com/cards/collection/"+mao
+  url = (API2)  
+  l = requests.get(url).json()
+  total = 1
+
+
+  
+  for i in l['cards']:
+    cn = "https://api.splinterlands.io/cards/find?ids=" + i['uid']
+    u = (cn)
+    c = requests.get(u).json()
+    for ca in c:
+      name = ca['details']['name']
+      new = name.replace(" ", "_").lower()
+      old = i['uid']
+
+      for content in co:
+        #sulod sa team folder
+        sulod = os.listdir(pf+content)
+        total = 1
+
+        for content_folder in sulod:
+          
+          counter = 0
+          #sulod sa mga folder sa team
+
+
+          with open(pf + content + "/" +content_folder, "r", encoding="utf-8") as file:
+            result = file.read()
+            counter  = result.count(old)
+            result = result.replace(old, new)
+      
+          with open(pf + content + "/" + content_folder, "w", encoding="utf-8") as newfile:
+            newfile.write(result)
+      
+          if counter:
+            total+=1
+
+###################################################            
 def cards():
   os.system('clear')
   f = open('acc.txt')
@@ -165,7 +214,7 @@ def cards():
 def zp():
   dl = input("Do you want to doqnload the Team file? [y/n]: ")
   if dl == "y" or dl == "Y" or dl == "yes" or dl == "YES" or dl == "Yes":
-    download = "1TM5tWU4oOD77j5B4_l7zb_gZzASnurc1"
+    download = "1Elm09EOkmVLnpDb8RF2cOfLa_pwyz8KA"
     print("Downloading Please wait...")
     r = requests.get("https://drive.google.com/uc?id=" + download + "&export=download")
     z = zipfile.ZipFile(io.BytesIO(r.content))
@@ -182,10 +231,11 @@ def menu():
   print ("\n\033[1;36m--------------------------")
   print ("(1) Fullname to UID")
   print ("(2) Id to Fullname")
-  print ("(3) Download Team File")
-  print ("(4) Delete Team File")
-  print ("(5) Check cards")
-  print ("(6) Exit")
+  print( "(3) Uid to Fullname")
+  print ("(4) Download Team File")
+  print ("(5) Delete Team File")
+  print ("(6) Check cards")
+  print ("(7) Exit")
   print ("--------------------------\n")
 
   option = input("\033[0m[>] Select Element: \033[0m")
@@ -199,26 +249,32 @@ def menu():
     except:
       print("[!] Please Download Team File first.")
 
-  if option == "2":
+  elif option == "2":
     scan2()
     print("Id to Fullname replaced, Success.")
     time.sleep(4)
     menu()
-    
 
   elif option == "3":
-    zp()
+    scan3()
+    print("UID to fullname replaced, Success.")
+    time.sleep(4)
+    menu()  
+    
 
   elif option == "4":
+    zp()
+
+  elif option == "5":
     os.system('rm -r team')
     menu()
 
-  elif option == "5":
+  elif option == "6":
     cards()
     input("\n\nScan Complete..Press Enter to continue...")
     menu()
 
-  elif option == "6":
+  elif option == "7":
     print("Thank you for using this Bot.")
     exit()
     
